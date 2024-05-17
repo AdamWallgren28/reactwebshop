3d3181518aff4cd6f01b8bb80608e30b61f584b7
Mattias Fucik <107649652+AdamWallgren28@users.noreply.github.com>
2024-05-17 13:05:02 +0200

    adds usestate to kvitto component

--- src/components/kvitto/kvitto.jsx
+++ src/components/kvitto/kvitto.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import  React, { useState } from "react";
 import OrderSummery from "../ordersummery/ordersummery";
 import CheckOutInfo from "../checkoutinfo/checkoutinfo";
 
 export default function Kvitto({cart}, {cartProducts}){
-    
+
+    const [forminfo, setForminfo] = useState({});
 
     return (
         <div>
             <h2>Your Order confirmation (Take a screenshot)</h2>
             <OrderSummery cart={cart} cartProducts={cartProducts} />    
-            <CheckOutInfo />
+            <CheckOutInfo {setForminfo} />
         </div>
     )
 }
