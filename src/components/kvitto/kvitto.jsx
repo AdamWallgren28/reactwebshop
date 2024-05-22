fcde90d6281e6942125fb0a6f81031ab8cc34d1c
Mattias Fucik <107649652+AdamWallgren28@users.noreply.github.com>
2024-05-22 13:54:29 +0200

    cleans up code structure of kvitto

--- src/components/kvitto/kvitto.jsx
+++ src/components/kvitto/kvitto.jsx
@@ -1,20 +1,3 @@
-// import  React, { useState } from "react";
-// import OrderSummery from "../ordersummery/ordersummery";
-// import CheckOutInfo from "../checkoutinfo/checkoutinfo";
-
-// export default function Kvitto({cart, cartProducts}){
-
-//     const [forminfo, setForminfo] = useState({});
-
-//     return (
-//         <div>
-//             <h2>Your Order confirmation (Take a screenshot)</h2>
-//             <OrderSummery cart={cart} cartProducts={cartProducts} />    
-//             <CheckOutInfo setForminfo={setForminfo} />
-//         </div>
-//     )
-// }
-
 import React, { useContext} from "react";
 import { ShopContext } from "../../context/shopcontext";
 import OrderSummery from "../ordersummery/ordersummery";
@@ -28,66 +11,37 @@ const Modal = ({ formData, closeModal }) => {
     const handleClose = () => {
         deleteCart();
         closeModal();
-        
     }
 
   return (
     <div className="fixed inset-0 z-50 flex items-start justify-center overflow-x-hidden overflow-y-auto backdrop-blur bg-black bg-opacity-30">
         <div className='my-4'>
-        <div className="bg-white rounded-lg shadow-lg">
-          <div className="p-4">
-            <h3 className="text-xl text-center font-bold mb-2">This is your recipt!</h3>
-            <h3 className="text-l text-center mb-6">(Take a screenshot)</h3>
-            <OrderSummery cart={cart} cartProducts={cartProducts} />
-            <div className='bg-gray-300 m-4 px-4 py-2 rounded-lg w-[50vw]'>
-                <div className="text-center font-bold my-2">Customer Information</div>
-                {/* <input className="mb-2">Full Name: {formData.fullName}</input>*/}
+            <div className="bg-white rounded-lg shadow-lg">
+                <div className="p-4">
+                    <h3 className="text-xl text-center font-bold mb-2">This is your recipt!</h3>
+                    <h3 className="text-l text-center mb-6">(Take a screenshot)</h3>
 
-                <input
-                    type="text"
-                    className="text-xs w-full px-2 py-1 border rounded-md mb-2"            
-                    value={formData.fullName}
-                />
-                <input
-                    type="text"
-                    className="text-xs w-full px-2 py-1 border rounded-md mb-2"            
-                    value={formData.address}
-                />
-                <div className='flex gap-2'>
-                    <input
-                        type="text"
-                        className="text-xs w-full px-2 py-1 border rounded-md mb-2"            
-                        value={formData.zip}
-                    />
-                    <input
-                        type="text"
-                        className="text-xs w-full px-2 py-1 border rounded-md mb-2"            
-                        value={formData.city}
-                    />
+                    <OrderSummery cart={cart} cartProducts={cartProducts} />
+
+                    <div className='bg-gray-300 m-4 px-4 py-2 rounded-lg w-[50vw]'>
+                        <div className="text-center font-bold my-2">Customer Information</div>
+                            <input type="text" value={formData.fullName} className="text-xs w-full px-2 py-1 border rounded-md mb-2"/>
+                            <input type="text" value={formData.address} className="text-xs w-full px-2 py-1 border rounded-md mb-2"/>
+                            <div className='flex gap-2'>
+                                <input type="text" value={formData.zip} className="text-xs w-full px-2 py-1 border rounded-md mb-2"/>
+                                <input type="text" value={formData.city} className="text-xs w-full px-2 py-1 border rounded-md mb-2"/>
+                            </div>
+                            <input type="text" value={formData.phone} className="text-xs w-full px-2 py-1 border rounded-md mb-2"/>
+                            <input type="text" value={formData.email} className="text-xs w-full px-2 py-1 border rounded-md mb-2"/>       
+                    </div>
+
+                    <div>
+                        <button onClick={handleClose} className="bg-gray-200 hover:bg-gray-300 border border-black border-opacity-25 text-black font-bold py-2 px-4 rounded">
+                            Close recipt
+                        </button>
+                    </div>             
                 </div>
-                <input
-                    type="text"
-                    className="text-xs w-full px-2 py-1 border rounded-md mb-2"            
-                    value={formData.phone}
-                />
-                <input
-                    type="text"
-                    className="text-xs w-full px-2 py-1 border rounded-md mb-2"            
-                    value={formData.email}
-                />
-       
-            </div>  
-            <div>
-                <button
-                className="bg-gray-200 hover:bg-gray-300 border border-black border-opacity-25 text-black font-bold py-2 px-4 rounded"
-                onClick={handleClose}
-                >
-                Close recipt
-                </button>
-            </div> 
-            
-          </div>
-        </div>
+            </div>
         </div>
     </div>
   );
